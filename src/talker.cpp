fb5c310fe650b3e53d2a545c6b64bbd747ce0705
Alex <alex@colon.fr>
2019-11-29 17:29:37 +0100

    [PI] Done

--- src/talker.cpp
+++ src/talker.cpp
@@ -3,61 +3,80 @@
 #include "uuv_gazebo_ros_plugins_msgs/FloatStamped.h"
 #include "sensor_msgs/FluidPressure.h"
 
+#include <iostream>
+#include <fstream>
 #include <sstream>
 
 #define DESIRED_DEPTH 15.00
 #define KP -0.3
+#define KI 0.5
+#define KD 0.4
+#define MAXIMUM_ANGLE 0.6
 
 float depth;
 float angle = 0.0;
 
 void chatterCallback(const sensor_msgs::FluidPressure& msg)
 {
-        float pressure = msg.fluid_pressure;
-        //Calcul for depth
-        depth = (pressure - 101.325) / 9.80638;
-        //Display results
-        ROS_INFO("Depth is: %f, desired depth is %f", depth, DESIRED_DEPTH);
-        ROS_INFO("And angle is : [%f]", angle);
+    float pressure = msg.fluid_pressure;
+    //Calcul for depth
+    depth = (pressure - 101.325) / 9.80638;
+    //Display results
+    ROS_INFO("Depth is: %f, desired depth is %f", depth, DESIRED_DEPTH);
+    ROS_INFO("And angle is : [%f]", angle);
 }
 
-float error()
+float output(float result_error)
 {
-    float error = DESIRED_DEPTH - depth;
-    float result_error = KP * error;
-    return abs(result_error) > 0.6 ? 0.6 : result_error;
+    if (abs(result_error) > MAXIMUM_ANGLE)
+        return MAXIMUM_ANGLE;
+    return result_error;
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "talker");
+    ros::init(argc, argv, "talker");
 
-	ros::NodeHandle n;
+    ros::NodeHandle n;
 
-        //PUBLISHERS
-	ros::Publisher chatter_pub1 = n.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>("/submarine/fins/0/input", 1000);
-	ros::Publisher chatter_pub2 = n.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>("/submarine/fins/1/input", 1000);
+    //PUBLISHERS
+    ros::Publisher chatter_pub1 = n.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>("/submarine/fins/0/input", 1000);
+    ros::Publisher chatter_pub2 = n.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>("/submarine/fins/1/input", 1000);
 
-        //SUBSCRIBER
-        ros::Subscriber sub = n.subscribe("/submarine/pressure", 1000, chatterCallback);
+    //SUBSCRIBER
+    ros::Subscriber sub = n.subscribe("/submarine/pressure", 1000, chatterCallback);
 
-	ros::Rate loop_rate(10);
+    ros::Rate loop_rate(10);
 
-	while (ros::ok())
-	{
-            //CALCULATE ERROR
-            angle = error();
 
-            //MSG AND PUBLISH
-            uuv_gazebo_ros_plugins_msgs::FloatStamped msg;
+    float error = 0.0;
+    float error_prior = 0.0;
+    float integral = 0.0;
+    float derivative = 0.0;
+    int iteration_time = 0.1;
+    while (ros::ok())
+    {
+        //CALCULATE ERROR
+        error = DESIRED_DEPTH - depth;
 
-            msg.data = angle;
+        integral = integral + (error * iteration_time);
 
-            chatter_pub1.publish(msg); 
-            chatter_pub2.publish(msg); 
+        derivative = (error - error_prior) / iteration_time;
 
-            ros::spinOnce();
-            loop_rate.sleep();
-        }
-        return 0;
+        angle = KP * error + KI * integral;
+
+        error_prior = error;
+
+        //MSG AND PUBLISH
+        uuv_gazebo_ros_plugins_msgs::FloatStamped msg; //Creation of msg
+
+        msg.data = output(angle); //If the angle is to big, reduce it
+
+        chatter_pub1.publish(msg); 
+        chatter_pub2.publish(msg); 
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
 }
